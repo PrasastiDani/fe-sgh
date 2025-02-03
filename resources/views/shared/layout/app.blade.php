c7a8905065ffdc32203430dbe4ba6cb75a0ff115
PrasastiDani <prasastidani@gmail.com>
2025-02-03 09:20:48 +0700

    remove tailwind head script

--- resources/views/shared/layout/app.blade.php
+++ resources/views/shared/layout/app.blade.php
@@ -9,7 +9,6 @@
 
     <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.1/dist/flowbite.min.css" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet" />
 
     <style>
